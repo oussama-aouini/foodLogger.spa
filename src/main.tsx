346be59874e83303a5f5a6a6187fe570eeda904a
oussama-aouini <oussama.aouini.dev@gmail.com>
2025-06-10 15:04:27 +0100

    initial architecture

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
+        audience: audience,
       }}
     >
       <App />
