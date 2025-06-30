88d20ea7a0cdbdd0e6126ec2c466ebef289fcc07
oussama-aouini <oussama.aouini.dev@gmail.com>
2025-06-30 12:56:26 +0100

    feat: add axios for API calls and implement food fetching in Foods component
    - Added axios as a dependency for making HTTP requests.
    - Updated Foods component to fetch food data from the API on mount.
    - Implemented loading and error states for better user experience.
    - Displayed fetched food items in a list.

--- src/pages/Foods.tsx
+++ src/pages/Foods.tsx
@@ -1,6 +1,8 @@
 import { useForm, type FieldValues } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import axios, { CanceledError } from "axios";
 
 const schema = z.object({
   name: z.string().min(3, { message: "Name must be at least 3 chars" }),
@@ -11,7 +13,35 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
+interface Food {
+  name: string;
+  calories: number;
+}
+
 const Foods = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+
+    axios
+      .get<Food[]>("https://localhost:7147/api/Food", {
+        signal: controller.signal,
+      })
+      .then((response) => setFoods(response.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => setIsLoading(false)); // best approach but doesn' work on strict mode must be in finally otherwise set immedialty cause axios get is async
+
+    return () => controller.abort();
+  }, []);
+
   const {
     register,
     handleSubmit,
@@ -47,6 +77,13 @@ const Foods = () => {
           Add Food
         </button>
       </form>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {foods.map((food) => (
+          <li key={food.name}>{food.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
